e2e5507f0f1cf93d666395352974682006dd6be6
Michel Douglas Grigoli <mdgrigoli@hotmail.comm.br>
2023-09-25 13:41:55 -0300

    XX

--- build-and-package.sh
+++ build-and-package.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-# Instale as dependências
 npm install
 
-# Construa o projeto React
 npm run build
-
-# Crie o arquivo .tgz
-npm pack
-
-# Mova o arquivo .tgz para um local onde o GitHub possa acessá-lo
-# mv meu-orcamento-online-*.tgz ./build
